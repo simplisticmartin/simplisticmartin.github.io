e6638286f2e26c8827453d1cffbf82b4237e8886
Martin <simplisticmartin@gmail.com>
2020-03-04 15:35:16 -0500

    Temp

--- projectFunctions.js
+++ projectFunctions.js
@@ -33,3 +33,19 @@ function openModalMod(fullProject) {
 {
 	return(`<object type="text/html" class="full-size-object" data=${link}></object>`);
 }
+$(".projects>li>a").on("click", function(e){
+  e.preventDefault();
+  var li=$(this).parent(),
+      li_height = li.height(),
+      details=li.find(".details"), 
+      details_height=details.height(),
+      new_height=details_height+40; 
+  li.toggleClass("current").animate({
+    paddingBottom: new_height
+  }, { duration: 200, queue: false }).siblings().removeClass("current");
+  $(".projects li:not(.current)").animate({
+    paddingBottom: '0'
+  }, { duration: 200, queue: false }).find(".details").slideUp(200);
+  $(".current").find(".details").slideDown(200);
+    
+});
--- projects.js
+++ projects.js
@@ -1,11 +1,11 @@
 var projectsList = [
     {
         projectName:"Life Quote Page",
-        icon: "Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        icon: "img/project-icons/quotepage.png",
         date:"Feb 2020",
         description: `This Project was designed for personal use and personal storage of quotes I use. This project utilizes`,
         projectLink:"/Projects/Quote-Generator/",
-        screenShot:"Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        screenShot:"img/screenshots/quote-page.png",
         techStack: "HTML, CSS3, JavaScript",
         awardStatus:"",
         reason:"",
@@ -13,4 +13,5 @@ var projectsList = [
     }
 ]
 
+//Projects/DNSandwiches/img/GameSprites/sandwich.png
 var numberOfProjects = projectsList.length;
