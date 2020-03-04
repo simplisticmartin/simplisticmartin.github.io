f8360ea05c6a9066f7d536c8760429501d51024c
Martin <simplisticmartin@gmail.com>
2020-03-04 00:32:55 -0500

    list building

--- projects.js
+++ projects.js
@@ -1,6 +1,15 @@
-let projectsList = {
-
-};
-
+let projectsList = [
+    {
+        projectName:"Life Quote Page",
+        icon: "Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        date:"Feb 2019",
+        description: `This Project was designed for personal use and personal storage of quotes I use. This project utilizes ${techStack}`,
+        projectLink:"/Projects/Quote-Generator/",
+        screenShot:"Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        techStack: "HTML, CSS3, JavaScript",
+        awardStatus:"",
+        reason:""
+    }
+]
 
 let numberOfProjects = projectsList.length;
