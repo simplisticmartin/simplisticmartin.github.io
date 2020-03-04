3096e120bcbd4443205dd1ad77e8fceb92f58803
Martin <simplisticmartin@gmail.com>
2020-03-04 01:00:20 -0500

    More design change

---
+++ projectFunctions.js
@@ -0,0 +1,4 @@
+let 
+for(let i = 0; i < numberOfProjects; i++){
+    
+}
--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ let projectsList = [
     {
         projectName:"Life Quote Page",
         icon: "Projects/DNSandwiches/img/GameSprites/sandwich.png",
-        date:"Feb 2019",
+        date:"Feb 2020",
         description: `This Project was designed for personal use and personal storage of quotes I use. This project utilizes ${techStack}`,
         projectLink:"/Projects/Quote-Generator/",
         screenShot:"Projects/DNSandwiches/img/GameSprites/sandwich.png",
