c7e0e60b7722d731123eeb27d3adeddf8045806b
Martin <simplisticmartin@gmail.com>
2020-03-04 16:02:51 -0500

    description

--- projects.js
+++ projects.js
@@ -3,13 +3,13 @@ var projectsList = [
         projectName:"Life Quote Page",
         icon: "img/project-icons/quotepage.png",
         date:"Feb 2020",
-        description: `This Project was designed for personal use and personal storage of quotes I use. This project utilizes`,
+        techStack: "HTML, CSS3, JavaScript",
+        gitRepo:"",
+        description: `This Project was designed for personal use and personal storage of quotes I use. This is a custom designed quote timeline with quotes from people with philosophies that resonates with me. Animation, Background Parallax Changes, responsive. Technology used: HTML,CSS,JavaScript`,
         projectLink:"/Projects/Quote-Generator/",
         screenShot:"img/screenshots/quote-page.png",
-        techStack: "HTML, CSS3, JavaScript",
         awardStatus:"",
-        reason:"",
-        gitRepo:""
+        reason:""
     }
 ]
 
