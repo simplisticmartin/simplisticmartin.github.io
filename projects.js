c01a719cd8d072283c976074a5133627844ffdce
Martin <simplisticmartin@gmail.com>
2020-03-05 02:28:15 -0500

    some Changes

--- projects.js
+++ projects.js
@@ -13,14 +13,14 @@ var projectsList = [
         reason:""
     },
         {
-        projectName:"GodZillow",
+        projectName:"NYC RENT PREDICTOR",
         icon: "img/project-icons/godzillow.png",
-        date:"Nov 2019",
-        techStack: "HTML, CSS3, JavaScript, P5JS, Bootstrap",
+        date:"2019",
+        techStack: "Python, Jupyter Notebook, Linear Regression, Random Forest",
         gitRepo:"",
-        description: `Made to understand web Development. Developed around 2016-2017. Contains many features such as a carousel, bootstrapped enhanced website. Simple target chasing game developed with P5.JS. Contains a quote generator. This is to perhaps create a fictious sandwich website. Designed for fun. Technology used: HTML, CSS3, JavaScript, P5JS, Bootstrap`,
-        projectLink:"Projects/DNSandwiches/",
-        screenShot:"/Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        description: `This Project was a machine learning project designed to create rent predictions in NYC. I used linear regression models, Random Forest. Cleaned Data to decrease Mean square error to much below 2 million MSE. Technology Used: Python, Jupyter Notebook, Linear Regression, Random Forest`,
+        projectLink:"https://github.com/simplisticmartin/Godzillow",
+        screenShot:"img/project-icons/godzillow.png",
         awardStatus:"",
         reason:""
     },
@@ -30,7 +30,8 @@ var projectsList = [
         date:"2019",
         techStack: "HTML, CSS3, JavaScript, NodeJS",
         gitRepo:"",
-        description: `This project was designed as a place for web designing services. This was done as an original design. This website was designed with SEO in mind and keeping track of constant attraction to the website. Techniques in SEO and social media were used to attract users. Still Work in Progress. Technology used: HTML,CSS,JavaScript, NodeJS, Photoshop, Illustrator`,
+        description: `This project was designed as a place for web designing services. This was done as an original design. This website was designed with SEO in mind and keeping track of constant attraction to the website. Techniques in SEO and social media were used to attract users. . I learned how to connect a contact form with nodejs server to send emails. I learned to use photoshop and other design tools to create images that fit with the overall theme of a website. I learned to make a website fully responsive so it would fit well on most devices. 
+        Still Work in Progress. Technology used: HTML,CSS,JavaScript, NodeJS, Photoshop, Illustrator`,
         projectLink:"http://clearsyn.com/",
         screenShot:"img/screenshots/clearsyn.png",
         awardStatus:"",
