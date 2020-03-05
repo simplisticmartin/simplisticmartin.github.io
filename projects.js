36400fd0856db064dbd7cad6fcd4baea5551d6a7
Martin <simplisticmartin@gmail.com>
2020-03-05 02:03:55 -0500

    first test

--- projects.js
+++ projects.js
@@ -1,8 +1,9 @@
 var projectsList = [
+    
     {
         projectName:"Life Quote Page",
         icon: "img/project-icons/quotepage.png",
-        date:"Feb 2020",
+        date:"2020",
         techStack: "HTML, CSS3, JavaScript",
         gitRepo:"",
         description: `This Project was designed for personal use and personal storage of quotes I use. This is a custom designed quote timeline with quotes from people with philosophies that resonates with me. Animation, Background Parallax Changes, responsive. Technology used: HTML,CSS,JavaScript`,
@@ -11,10 +12,22 @@ var projectsList = [
         awardStatus:"",
         reason:""
     },
+        {
+        projectName:"GodZillow",
+        icon: "img/project-icons/godzillow.png",
+        date:"Nov 2019",
+        techStack: "HTML, CSS3, JavaScript, P5JS, Bootstrap",
+        gitRepo:"",
+        description: `Made to understand web Development. Developed around 2016-2017. Contains many features such as a carousel, bootstrapped enhanced website. Simple target chasing game developed with P5.JS. Contains a quote generator. This is to perhaps create a fictious sandwich website. Designed for fun. Technology used: HTML, CSS3, JavaScript, P5JS, Bootstrap`,
+        projectLink:"Projects/DNSandwiches/",
+        screenShot:"/Projects/DNSandwiches/img/GameSprites/sandwich.png",
+        awardStatus:"",
+        reason:""
+    },
     {
         projectName:"Clearsyn.com",
         icon: "img/project-icons/clearsyn.png",
-        date:"Mar 2019",
+        date:"2019",
         techStack: "HTML, CSS3, JavaScript, NodeJS",
         gitRepo:"",
         description: `This project was designed as a place for web designing services. This was done as an original design. This website was designed with SEO in mind and keeping track of constant attraction to the website. Techniques in SEO and social media were used to attract users. Still Work in Progress. Technology used: HTML,CSS,JavaScript, NodeJS, Photoshop, Illustrator`,
@@ -24,9 +37,21 @@ var projectsList = [
         reason:""
     },
     {
+        projectName:"Dodgeball Game",
+        icon: "img/project-icons/Default.png",
+        date:"2018",
+        techStack: "",
+        gitRepo:"",
+        description: `Promo page in progress. Video Incoming. Technology Used: Unreal Engine, Blueprints, C++, Blender`,
+        projectLink:"Projects/game-promo",
+        screenShot:"img/project-icons/Default.png",
+        awardStatus:"",
+        reason:""
+    },
+    {
         projectName:"Decently Nice Sandwiches",
         icon: "/Projects/DNSandwiches/img/GameSprites/sandwich.png",
-        date:"Jan 2016",
+        date:"2016",
         techStack: "HTML, CSS3, JavaScript, P5JS, Bootstrap",
         gitRepo:"",
         description: `Made to understand web Development. Developed around 2016-2017. Contains many features such as a carousel, bootstrapped enhanced website. Simple target chasing game developed with P5.JS. Contains a quote generator. This is to perhaps create a fictious sandwich website. Designed for fun. Technology used: HTML, CSS3, JavaScript, P5JS, Bootstrap`,
