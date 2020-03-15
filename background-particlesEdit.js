ca0fece2eb7bd552bea1cd1542264c47cf5f50eb
Martin <simplisticmartin@gmail.com>
2020-03-15 01:22:40 -0400

    changes

--- background-particlesEdit.js
+++ background-particlesEdit.js
@@ -176,7 +176,7 @@
   particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 150,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
